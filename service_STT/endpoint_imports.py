ce66f81828ff162628ed98ff206c8d3cc4576ca5
Coricos <meryll.dindin@student.ecp.fr>
2019-06-28 11:54:40 -0700

    Build Flask service for speech-to-text

---
+++ service_STT/endpoint_imports.py
@@ -0,0 +1,11 @@
+# Author:  DINDIN Meryll
+# Date:    28 June 2019
+# Project: AsTeR
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import Response
+from flask_restful import Api
+from flask_restful import Resource
+from werkzeug.utils import secure_filename
---
+++ service_STT_endpoint.py
@@ -0,0 +1,36 @@
+# Author:  DINDIN Meryll
+# Date:    28 June 2019
+# Project: AsTeR
+
+from service_STT.speech_apis import *
+from service_STT.endpoint_imports import *
+
+# Defines the service APIs
+
+api_IBM = Voice_IBM()
+api_Rev = Voice_Rev()
+api_GGC = Voice_GGC()
+
+# Run Flask local server
+
+if __name__ == '__main__':
+
+    app = Flask('STT')
+    api = Api(app)
+
+    @app.route('/speech-to-text', methods=['POST'])
+    def run_service():
+
+        fle = request.files['audio_file']
+        nme = '/'.join(['./.tmp', secure_filename(fle.filename)])
+        fle.save(nme)
+
+        arg = dict(request.args)
+        if arg['api_type'] == 'IBM': req = api_IBM.request(nme)
+        if arg['api_type'] == 'Rev': req = api_Rev.request(nme)
+        if arg['api_type'] == 'GGC': req = api_GGC.request(nme)
+
+        arg = {'status': 200, 'mimetype': 'application/json'}
+        return Response(response=json.dumps(req), **arg)
+
+    app.run(host='127.0.0.1', port='8080', threaded=True)
